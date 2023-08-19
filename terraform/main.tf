127134fad9b0a12b49f2d7163913855a56c06083
Deepak Kumar Sharma <71060144+deep1757@users.noreply.github.com>
2023-08-19 13:34:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,24 +6,12 @@ resource "aws_instance" "example" {
   ami           = "ami-04e35eeae7a7c5883" # Replace with your desired AMI
   instance_type = "t2.micro"
 
+
   tags = {
     Name = "Terraform CICD"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install -y httpd",
-      "sudo systemctl start httpd",
-      "sudo systemctl enable httpd",
-      "echo '<h1>Hello, this is your website!</h1>' | sudo tee /var/www/html/index.html"
-    ]
   }
 }
 
 output "public_ip" {
   value = aws_instance.example.public_ip
-}
-
-output "website_url" {
-  value = "http://${aws_instance.example.public_ip}:80"
 }

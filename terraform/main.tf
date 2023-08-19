87c02c9720c0fad48de3f1a81520213000f3dabf
k21academyuk <contactus@k21academy.com>
2023-08-19 11:05:29 +0530

    Setting up CI/CD pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2" # Replace with your desired region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04e35eeae7a7c5883" # Replace with your desired AMI
+  instance_type = "t2.micro"
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}

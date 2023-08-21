92863303e20e58bb8919165e6eca34d4a26c4681
Deepak Kumar Sharma <71060144+deep1757@users.noreply.github.com>
2023-08-21 13:22:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-04e35eeae7a7c5883" # Replace with your desired AMI
+  ami           = "ami-0e0f3d4588f992288" # Replace with your desired AMI
   instance_type = "t2.micro"
-
 
   tags = {
     Name = "Terraform CICD"
   }
+}
+
+# Allow incoming traffic on port 80 for the default security group
+resource "aws_security_group_rule" "http_ingress" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"] # Allowing traffic from all sources (for demonstration purposes)
+  security_group_id = aws_instance.example.security_groups[0]
 }
 
 output "public_ip" {
